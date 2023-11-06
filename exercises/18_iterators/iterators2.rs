4015de5ddd46bf0c9f743950f4afef0901a79c43
Raymon Roos <raymon.roos@hotmail.com>
2023-11-06 20:47:04 +0100

    iterators: finished section
    Really cool stuff, makes for really concise yet semantic code, feels
    pretty magical.

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -6,17 +6,17 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
-    match c.next() {
+    let capital = match c.next() {
         None => String::new(),
-        Some(first) => ???,
-    }
+        Some(first) => first.to_ascii_uppercase().to_string(),
+    };
+
+    capital + c.as_str()
 }
 
 // Step 2.
@@ -24,7 +24,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.into_iter().map(|s| capitalize_first(s)).collect()
 }
 
 // Step 3.
@@ -32,7 +32,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    capitalize_words_vector(words).join("")
 }
 
 #[cfg(test)]
--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -15,6 +13,23 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    // I really wanted to use recursion with tail call optimization (should have read the comment
+    // better, but I got excited), but couldn't figure out how to build in an accumulator without
+    // changing the function signature, 'cause I'd rather not require the caller to have to know
+    // what number to pass as second argument. That should be an implementation detail. I came
+    // across this in a blog: https://101wiki.softlang.org/Concept:Accumulator_parameter
+
+    fn acc(current: u64, fact: u64) -> u64 {
+        match current {
+            ..=1 => fact,
+            _ => acc(current - 1, current * fact),
+        }
+    }
+
+    acc(num, 1)
+    // Or just do this, but I didn't come up with it:
+    // (1..=num).product()
 }
 
 #[cfg(test)]
